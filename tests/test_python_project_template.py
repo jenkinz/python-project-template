6dc11ae11f2d69f1b1252afd75533ec509164a0a
Brian Jenkins <bj@jenkinz.com>
2022-03-23 12:30:16 -0700

    Add black and flake8 as project dependencies

--- tests/test_python_project_template.py
+++ tests/test_python_project_template.py
@@ -1,6 +1,10 @@
 from python_project_template import __version__
 from python_project_template.math import multiply_two_numbers
 
+TEST_STRING = (
+    "This is a very long string to test auto reformatting with black. It should get reformatted automatically...."
+)
+
 
 def test_version():
     assert __version__ == "0.1.0"
